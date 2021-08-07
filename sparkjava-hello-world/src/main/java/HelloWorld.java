5c0efad364bee03ccefeeed27bcb7cbc81ae6310
raspa <raspa@DESKTOP-IJ6MFO7.mshome.net>
2021-08-07 18:20:04 +0530

    gjghj

--- sparkjava-hello-world/src/main/java/HelloWorld.java
+++ sparkjava-hello-world/src/main/java/HelloWorld.java
@@ -5,7 +5,7 @@ import static spark.Spark.get;
 public class HelloWorld implements SparkApplication {
 	public static void main(String[] args) {
 		new HelloWorld().init();
-	}
+	} 
 
 	@Override
 	public void init() {  
